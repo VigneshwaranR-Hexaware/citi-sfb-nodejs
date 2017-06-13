a7a6b67b3fdc4829936fa9f6b77b566378723a57
hexinnov <htlp001@gmail.com>
2017-06-13 18:04:45 +0530

    Up and Running

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var restify = require('restify');
 var apiairecognizer = require('api-ai-recognizer');
 //Dependencies
 
-//var savedAddress; //Variable to save the session address of user for Proactive messaging
-
+var savedAddress;
+var conversationIndex=0; //Conversation Counter
+//Global Session Holder variables
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -14,63 +15,156 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+
 // Create connector and listen for messages
 var connector = new builder.ChatConnector({
    appId: "72308ad8-d853-4775-8019-d2fee6686eb2",
    appPassword: "4DXyKmdrpejRao5fTZeH8NF"
 });
 
-server.post('/', connector.listen());
 
-//Configure the Bot Object
+
+server.post('/', connector.listen());
+//POST Call Handler
+
+
 var bot = new builder.UniversalBot(connector);
 var recognizer = new apiairecognizer('2ce0da1c25354c2bb16ef5cb0f61e43f');
 bot.recognizer(recognizer);
-var intents = new builder.IntentDialog({ recognizers: [recognizer] });
-
-console.log("Getting Ready");
-bot.dialog('/', intents); //Listening to messages as Query Parameters with POST call
-
-console.log("Intent----"+JSON.stringify(intents));
-
-intents.matches('WebhookIntent', [
-    function (session, args, next) {
-       //savedAddress = session.message.address;
-       global.savedAddress = session.message.address;
-       console.log("Global Saved Address  : "+session.message.address);
-       console.log("Webhook Intent Triggered");
-       console.log("Args : "+JSON.stringify(args));
-       session.send('Webhook Intent Called API.AI', session.message.text);
-
-    }//Response for Webhook Intent Returned
-]),
+//Create Bot Object
 
 
-intents.matches('inquiryIntent', [
-    function (session, args, next) {
-      console.log("Webhook Intent Triggered");
-      console.log("Args : "+JSON.stringify(args));
-      session.send('Inquiry Intent Called API.AI', session.message.text);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
 
-    }//Response for inquiry Intent Returned
-]);
+                .matches('WebhookIntent', [
+                function (session, args, next) {
+                    console.log("Webhook Intent Called");
+                    console.log("Args : "+args);
+
+                    global.savedAddress = session.message.address;
+
+                    console.log("savedAddress : "+savedAddress);
+                    session.send('Webhook Intent Called API.AI');
+                    session.endDialog();
+                }
+              ])//Webhook Intent Fired
+
+                .matches('testIntent', [
+                    function (session, args, next) {
+                       //session.send('Inquiry Intent Called API.AI', session.message.text);
+                       console.log("Test Intent Fired");
+                       console.log("Args : "+args);
+                        //console.log("New Conversation Intent Called 1.0");
+                        session.send('Test Intent Response from API.AI')
+                        //global.savedAddress = session.message.address;
+                        //startNewConversation(savedAddress);
+                  }
+                ])//test Intent Fired
+
+                .matches('inquiryIntent', [
+                    function (session, args, next) {
+                       console.log("Inquiry Intent Called");
+                       console.log("Args : "+args);
+                       session.send('Inquiry Intent Called API.AI');
+
+                  }
+                ])//Inquiry Intent Fired
 
 
+                .onDefault((session) => {
+                        console.log("Fallback Intent Fired");
+                        session.send("FallbackIntent").endDialog();
+                  //Fallback Intent Fired
+});
 
+bot.dialog('/', intents);
+
+// //console.log("INtent----"+JSON.stringify(intents));
+// bot.dialog('/profile', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         session.userData.name = results.response;
+//         session.endDialog();
+//     }
+// ]);
+//
+// bot.dialog('/helloword', [
+//
+//     function (session, results) {
+//         session.send('Hello User');
+//         conversationIndex=1;
+//
+//     }
+// ]);
 
 server.get('/', (req, res, next) => {
-    var savedAddress=global.savedAddress;
     sendProactiveMessage(savedAddress);
-    res.send('Notification Trigger Successful');
+    res.send('Proactive Notification triggered');
     next();
-  }
-);//GET call response sent on HTTP GET calls the Postback URL
+  }//Proavtive Notifications
+  //GET Call Response Header Definitions
+);
 
 
 function sendProactiveMessage(address) {
   var msg = new builder.Message().address(address);
-  msg.text('Hello, this is an external notification. Hence, Proactive Messaging Successful');
+  msg.text('Hello, this is a notification from an external Call');
   msg.textLocale('en-US');
-  console.log("msg : "+msg);
   bot.send(msg);
+}//GET Call Notification Function
+
+
+function startNewConversation(add){
+
+    console.log("LastConversationID" + add.conversation.id);
+    var address = { id: '7',
+  channelId: 'skypeforbusiness',
+  user:
+   { id: 'sip:ashishdubey@rnddevlab.onmicrosoft.com',
+     name: 'ashish Dubey' },
+  conversation:
+   { isGroup: true,
+     id: add.conversation.id },
+  bot:
+   { id: 'sip:micro@rnddevlab.onmicrosoft.com',
+     name: 'sip:micro@rnddevlab.onmicrosoft.com' },
+  serviceUrl: 'https://webpoolsg20r04.infra.lync.com/platformservice/tgt-c98ad92f33ed54008a30cbb5bd5314f9/botframework' };
+
+        console.log('Starting new conversation for address:', address);
+        var newConversationAddress = Object.assign({}, address);
+     //   delete newConversationAddress.conversation;
+        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
+                if (err) {
+                    // error ocurred while starting new conversation. Channel not supported?
+                    bot.send(new builder.Message()
+                        .text('This channel does not support this operation: ' + err.message)
+                        .address(address));
+                }
+            });
+
+
+
 }
+
+//
+// bot.dialog('survey', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hello... What\'s your name?');
+//     },
+//     function (session, results) {
+//         session.userData.name = results.response;
+//         builder.Prompts.number(session, 'Hi ' + results.response + ', How many years have you been coding?');
+//     },
+//     function (session, results) {
+//         session.userData.coding = results.response;
+//         builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
+//     },
+//     function (session, results) {
+//         session.userData.language = results.response.entity;
+//         session.endDialog('Got it... ' + session.userData.name +
+//             ' you\'ve been programming for ' + session.userData.coding +
+//             ' years and use ' + session.userData.language + '.');
+//     }
+// ]);
