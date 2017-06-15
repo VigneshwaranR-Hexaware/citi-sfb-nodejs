b039b83dd32d148e05d6230965e3e8235827a9fb
hexinnov <htlp001@gmail.com>
2017-06-15 10:03:19 +0530

    Up and Running

--- app.js
+++ app.js
@@ -167,13 +167,15 @@ var intents = new builder.IntentDialog({ recognizers: [recognizer] })
                       global.entityValue=entityValue;
                       console.log("Data Specific Entity Value : "+entityValue);
 
-                      var projectNameObtained = builder.EntityRecognizer.findEntity(args.entities, 'projectNameEntity');
-                      console.log("Project Name Entity Lot : "+JSON.stringify(projectNameObtained));
-                      var projectValue=projectNameObtained.entity;
-                      global.projectValue=projectValue;
-                      console.log("Project Name Entity Value : "+projectValue);
 
                       if(projectValue!=null){
+
+                        var projectNameObtained = builder.EntityRecognizer.findEntity(args.entities, 'projectNameEntity');
+                        console.log("Project Name Entity Lot : "+JSON.stringify(projectNameObtained));
+                        var projectValue=projectNameObtained.entity;
+                        global.projectValue=projectValue;
+                        console.log("Project Name Entity Value : "+projectValue);
+
                         var responseString="Here is your report data.";
                         session.send(responseString);
                       }//If User says project name along with inquiry request
